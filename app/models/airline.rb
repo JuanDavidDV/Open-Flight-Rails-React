427f19cd798a8d45f7e611a6cfdca738cfe3860c
JuanDavidDV <juandadazav@gmail.com>
2025-06-02 08:02:22 -0400

    setup action in Airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,13 @@
 class Airline < ApplicationRecord
   has_many :reviews
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
+
+  before_create :slugify
+  # Takes airlines name and converts it into a URL safe slug, and then sets is a the slug value
+  def slugify
+    self.slug = name.parameterize
+  end
 end
