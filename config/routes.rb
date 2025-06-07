a320de3eedd5ed64b54d7004b527155de38dd215
JuanDavidDV <juandadazav@gmail.com>
2025-06-07 15:21:29 -0400

    add namespaces to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root "pages#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: [ :create, :destroy ]
+    end
+  end
 end
