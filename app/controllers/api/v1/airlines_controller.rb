a9fafb5ac5b65671e08ded61ea394d3e60c384ec
JuanDavidDV <juandadazav@gmail.com>
2025-06-16 09:52:31 -0400

    create update method

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -23,6 +23,16 @@ module Api
         end
       end
 
+      def update
+        @airline = Airline.find_by(slug: params[:slug])
+
+        if @airline.update(airline_params)
+          render json: AirlineSerializer.new(@airline).serialized_json
+        else
+          render json: { error: airline.errors.messages }, statsu: 404
+        end
+      end
+
       private
 
       def airline_params
