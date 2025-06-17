fcf5e2901dbc0e3d7b7a0af4784102334c10ace1
JuanDavidDV <juandadazav@gmail.com>
2025-06-17 17:48:14 -0400

    create provate action review_params

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,14 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
-      
+      def create
+        @review = Review.new(review_params)
+      end
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
     end
   end
 end
